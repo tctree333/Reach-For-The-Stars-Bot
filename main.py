626bd71f1706fc34b04dba15ddc8ff40f2be725f
Tomi Chen <tctree333@users.noreply.github.com>
2020-03-15 16:59:08 -0700

    uhhhh

---
+++ main.py
@@ -0,0 +1,36 @@
+import sciolyid as bot
+import credentials
+
+bot.setup(
+    bot_description = "Star ID - A SciOly Discord Bot for aspiring astronomers",
+    bot_signature = "Star ID - An Astronomy Bot",
+    prefixes = ["s!", "s.", "s*", "S!", "S.", "S*"],
+    id_type = "star",
+    short_id_type = "st",
+    name = "star-id",
+
+    github_image_repo_url = "https://github.com/tctree333/Reach-For-The-Stars-Images.git",
+    list_dir = "lists",
+    wikipedia_file = "wikipedia.txt",
+    alias_file = "aliases.txt",
+
+    logs = True,
+    log_dir = "logs",
+    file_folder = "bot_files",
+    data_dir = "data",
+
+    invite = "This bot is currently not avaliable outside the support server.",
+    support_server = "https://discord.gg/Y2xwJSh",
+    authors = "person_v1.32, with code from hmmm and EraserBird, and with help from Naddle and whaledemon",
+    source_link = "https://github.com/tctree333/Reach-For-The-Stars-Bot",
+
+    secrets = credentials.credentials,
+    id_groups = True,
+    category_name = "Space Thing",
+    category_aliases = {"constellations": ["constellations", "constellation", "cst", "c"],
+    "dsos": ["dsos", "dso", "d"],
+    "stars": ["stars", "star", "s"]},
+
+)
+
+bot.start()
