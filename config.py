d37ef01a80b03fc037c123d25731e09670c2f0d8
Tomi Chen <tctree333@users.noreply.github.com>
2021-12-27 20:19:48 -0800

    update invite

--- config.py
+++ config.py
@@ -4,27 +4,24 @@ config = {
     "prefixes": ["s!", "s.", "s*", "S!", "S.", "S*"],
     "id_type": "stars",
     "github_image_repo_url": "https://github.com/tctree333/Reach-For-The-Stars-Images.git",
-    "support_server": "https://discord.gg/Y2xwJSh",
+    "support_server": "https://discord.gg/2HbshwGjnm",
     "source_link": "https://github.com/tctree333/Reach-For-The-Stars-Bot",
     "members_intent": True,
-
     "category_name": "Space Thing",
     "default_state_list": "RFTS",
-
     "client_id": 666825281389985803,
     "base_image_url": "https://raw.githubusercontent.com/tctree333/Reach-For-The-Stars-Images/master/",
     "validation_repo_url": "https://github.com/person-v132/SciOlyID-Images.git",
     "validation_repo_dir": "rfts",
-
     "name": "star-id",
     "logs": True,
     "restricted_list_dir": "restricted/",
     "bot_files_dir": "bot_files/",
     "short_id_type": "s",
-    "invite": "https://discord.com/oauth2/authorize?client_id=666825281389985803&permissions=268486656&scope=bot" +
-              "\n\nPlease note that this bot, while fully featured, is not officially supported. Image quality " +
-              "has not been checked, and many items are missing images. In addition, this bot may be used for " +
-              "testing purposes and we make no guarantee that the bot will be in working condition.",
+    "invite": "https://discord.com/oauth2/authorize?client_id=666825281389985803&permissions=268486656&scope=bot"
+    + "\n\nPlease note that this bot, while fully featured, is not officially supported. Image quality "
+    + "has not been checked, and many items are missing images. In addition, this bot may be used for "
+    + "testing purposes and we make no guarantee that the bot will be in working condition.",
     "authors": "person_v1.32, with code from hmmm and EraserBird, and with help from Naddle and WhaleDemon",
     "id_groups": True,
     "category_aliases": {
